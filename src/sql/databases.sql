589b0f8b56698693f77e51777a3aa9961e3a27d7
nadun <weerakkody.kn@gmail.com>
2024-09-20 10:07:05 +0530

    change in file structure

--- src/sql/databases.sql
+++ src/sql/databases.sql
@@ -19,3 +19,12 @@ CREATE TABLE `students` (
   `registered_date` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`student_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `notification` (
+  `notification_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`notification_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
