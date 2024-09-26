b685b286adf20ebaaa1087b6a7363ee15b980e97
AroshXR <aroshanasandeep454@gmail.com>
2024-09-26 10:54:21 +0530

    added php

--- src/sql/databases.sql
+++ src/sql/databases.sql
@@ -40,5 +40,16 @@ CREATE TABLE `admin` (
   PRIMARY KEY (`admin_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+
 INSERT INTO admin (admin_id, name, password)
 VALUES ("AD236034", "john Doe", "john123");
+
+CREATE TABLE `Exams` (
+    `exam_id` INT AUTO_INCREMENT,
+    `exam_name` VARCHAR(255) NOT NULL,
+    `assigned_examiner` VARCHAR(255) NOT NULL,
+    `exam_deadline` DATE NOT NULL,
+    `exam_password` VARCHAR(255) NOT NULL,
+    `examiner_email` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`exam_id`)
+);
