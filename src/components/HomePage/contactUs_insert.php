478788eaa01933e40dad3fbe17c51f257a8d2eaa
Ima318 <tharini.imalka@gmail.com>
2024-10-02 12:22:00 +0530

    contact

--- src/components/HomePage/contactUs_insert.php
+++ src/components/HomePage/contactUs_insert.php
@@ -6,12 +6,13 @@ if ($conn->connect_error) {
   die('Connection Error : ' . $conn->connect_error);
 }
 
+$contact_ID=$_POST["ID"];
 $contact_name= $_POST["name"];
 $contact_phone = $_POST["phone"];
 $contact_message = $_POST["message"];
 
 
-$sql = "INSERT INTO message VALUES('$contact_name','$contact_phone','$contact_message')";
+$sql = "INSERT INTO message VALUES('$contact_ID','$contact_name','$contact_phone','$contact_message')";
 
 if($conn->query($sql))
 {
@@ -20,6 +21,8 @@ else{
     echo "Error".$conn->error;
 }
 
+header("Location: contactus.php");
+exit();
 $conn->close();
 
 ?>
