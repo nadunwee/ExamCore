f041f65e6c118604dbab272e73694d4156f4539f
Ima318 <tharini.imalka@gmail.com>
2024-10-08 17:23:19 +0530

    config

--- src/php/config.php
+++ src/php/config.php
@@ -1,13 +1,10 @@
 <?php
   $conn = new mysqli('localhost', 'root', '', 'exam_core');
-//to create the connection- new mysquli- query $conn is the variable
+
 
   if ($conn->connect_error) {
     die('Connection Error : ' . $conn->connect_error);
   }
-  //a function to remove the connection-die
+ 
   
-  else {
-    echo "not successful!";
-  }
 ?>
